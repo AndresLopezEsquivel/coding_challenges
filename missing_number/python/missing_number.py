51459329a09b8c7d51dcf197cbecd81f24572284
your_github_username <valentinaleonh8@gmail.com>
2025-01-11 21:51:58 -0600

    Implement Missing Number solution

--- missing_number/python/missing_number.py
+++ missing_number/python/missing_number.py
@@ -8,5 +8,18 @@ def generate_sequence(n):
     
     return sequence, missing_number
 
-sequence, missing_number = generate_sequence(3)
-print(f"Sequence: {sequence}, removed number: {missing_number}")
+# sequence, missing_number = generate_sequence(4)
+# print(f"Sequence: {sequence}, removed number: {missing_number}")
+
+def find_missing_number(sequence, n):
+    total_sum = n * (n + 1) // 2 # find the total sum
+    current_sum = sum(sequence) # calculate the current_sum
+    the_number = total_sum - current_sum # the missing number
+    return the_number
+
+n = 4
+sequence, the_number = generate_sequence(n)
+print(f"sequence: {sequence}, removed number: {the_number}")
+
+calculated_missing = find_missing_number(sequence, n)
+print(f"this is the calculated missing number: {calculated_missing}")
