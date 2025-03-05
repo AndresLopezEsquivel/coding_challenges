397960edd67ae4c6ad9002cded219cff0b0aa8db
your_github_username <valentinaleonh8@gmail.com>
2025-03-04 22:16:57 -0600

    Implement GCD of Strings using Euclidean Algorithm and add test cases

--- LeetCode/1071_GreatestCommonDivisorOfStrings/javascript/main.js
+++ LeetCode/1071_GreatestCommonDivisorOfStrings/javascript/main.js
@@ -1,22 +1,28 @@
-const gcdOfStrings = function(str1, str2) {
-    // the eucledian algorithm: 
+// i recommend taking a look at the following references:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring
+// https://www.w3schools.com/dsa/dsa_ref_euclidean_algorithm.php
+
+const gcdOfStrings = function(str1, str2) { 
     // gcd(a, b) = gcd(b, b%a);
     let a = str1.length;
     let b = str2.length;
     let remainder;
 
+
+    // compute the GCD of the string lengths using Euclidean algorithm
     while(b != 0) {
         remainder = a % b;
         a = b;
         b = remainder;
     } 
 
-    const gcd = str1.substring(0, a) // 
+    // extract the potencial GCD substring
+    const gcdSubstring = str1.substring(0, a)
 
-    // check the docs for reference of the .repeat() method:
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
-    if(str1 === gcd.repeat(str1.length / a) && str2 === gcd.repeat(str2.length / a)) {
-        return gcd;
+    // check if both strings can be constructed using the GCD substring
+    if(str1 === gcdSubstring.repeat(str1.length / a) && str2 === gcdSubstring.repeat(str2.length / a)) {
+        return gcdSubstring;
     } else {
         return "";
     }
