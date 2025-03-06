e483af2e08899e09afe5d92b5dea9b153599c482
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2025-03-05 21:46:14 -0600

    Enhancing solution to the 230th LeetCode problem

--- LeetCode/230_KthSmallestElementInBST/cpp/main.cpp
+++ LeetCode/230_KthSmallestElementInBST/cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 class Node
 {
@@ -8,23 +9,27 @@ class Node
   int value;
   Node() : value{0}, left{nullptr}, right{nullptr} {}
   Node(int value) : value{value}, left{nullptr}, right{nullptr} {}
+  ~Node() {
+    delete left;
+    delete right;
+  }
 };
 
-void in_order_traversal(Node* node, int& k, int& min)
+void in_order_traversal(Node* node, int& k, int& kth_smallest_value)
 {
   if(!node) return;
-  in_order_traversal(node->left, k, min);
+  in_order_traversal(node->left, k, kth_smallest_value);
   // Once we reach the smallest item, we need to iterate k times to reach the kth smallest
   if(--k == 0)
-    min = node->value;
-  in_order_traversal(node->right, k, min);
+    kth_smallest_value = node->value;
+  in_order_traversal(node->right, k, kth_smallest_value);
 }
 
 int kth_smallest(Node* root, int k)
 {
-  int min = -1;
-  in_order_traversal(root, k, min);
-  return min;
+  int kth_smallest_value = -1;
+  in_order_traversal(root, k, kth_smallest_value);
+  return kth_smallest_value;
 }
 
 int main()
@@ -41,7 +46,11 @@ int main()
   root_2->left->right = new Node{4};
   root_2->left->left->left = new Node{1};
 
-  std::cout << "root_1 : " << kth_smallest(root_1, 1) << std::endl;
-  std::cout << "root_2 : " << kth_smallest(root_2, 3) << std::endl;
+  std::cout << "The 1st smallest element in root_1: " << kth_smallest(root_1, 1) << std::endl;
+  std::cout << "The 3rd smallest element in root_2: " << kth_smallest(root_2, 3) << std::endl;
+
+  delete root_1;
+  delete root_2;
+
   return 0;
 }
