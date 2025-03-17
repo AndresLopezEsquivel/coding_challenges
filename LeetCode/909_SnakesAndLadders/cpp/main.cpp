ab2e2b619c4921cef3a52e7b0cb7cd639c6d4e55
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2025-03-17 16:51:11 -0600

    Implement BFS to traverse snakes and ladders board (LeetCode 909 problem)

--- LeetCode/909_SnakesAndLadders/cpp/main.cpp
+++ LeetCode/909_SnakesAndLadders/cpp/main.cpp
@@ -1,4 +1,6 @@
+#include <deque>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 std::ostream& operator<<(
@@ -36,32 +38,64 @@ std::pair<int,int> get_indexes_of(
   return std::make_pair(r,c);
 }
 
-std::vector<std::pair<int,int>> get_adj(
-  int r,
-  int c,
-  std::vector<std::vector<int>>& board)
+void snakes_and_ladders(std::vector<std::vector<int>>& board)
 {
-
+  int board_h = board.size();
+  int board_w = board.front().size();
+  std::vector<bool> visited ((board_h * board_w) + 1, false);
+  std::deque<int> q;
+  q.push_back(1);
+  visited[1] = true;
+  while(!q.empty())
+  {
+    int curr_pos = q.front();
+    q.pop_front();
+    std::cout << curr_pos << std::endl;
+    std::cout << "[ ";
+    for(size_t i = 1; i < 7; i++)
+    {
+      int next_pos = curr_pos + i;
+      if(next_pos > board_h * board_w) continue;
+      std::pair<int,int> next_idx = get_indexes_of(next_pos, board);
+      if(board[next_idx.first][next_idx.second] != -1)
+        next_pos = board[next_idx.first][next_idx.second];
+      if(!visited[next_pos])
+      {
+        visited[next_pos] = true;
+        q.push_back(next_pos);
+      }
+      std::cout << next_pos << " ";
+    }
+    std::cout << "]" << std::endl;
+  }
 }
 
 int main()
 {
-  std::vector<std::vector<int>> board_1 {
-    // {-1, 2, -1},
-    // {-1, 9, 7},
-    // {-1, 5, -1}
-    {-1, -1, -1},
-    {-1, -1, -1}
+  std::vector<std::vector<int>> board_1
+  {
+    {-1, 2, -1},
+    {-1, 9, 7},
+    {-1, 5, -1}
   };
 
+  std::vector<std::vector<int>> board_2
+  {
+    {-1,-1,-1,-1,-1,-1},
+    {-1,-1,-1,-1,-1,-1},
+    {-1,-1,-1,-1,-1,-1},
+    {-1,35,-1,-1,13,-1},
+    {-1,-1,-1,-1,-1,-1},
+    {-1,15,-1,-1,-1,-1}
+  };
 
-
-  std::cout << get_indexes_of(1,board_1) << std::endl;
-  std::cout << get_indexes_of(2,board_1) << std::endl;
-  std::cout << get_indexes_of(3,board_1) << std::endl;
-  std::cout << get_indexes_of(4,board_1) << std::endl;
-  std::cout << get_indexes_of(5,board_1) << std::endl;
-  std::cout << get_indexes_of(6,board_1) << std::endl;
+  // std::cout << get_indexes_of(1,board_1) << std::endl;
+  // std::cout << get_indexes_of(2,board_1) << std::endl;
+  // std::cout << get_indexes_of(3,board_1) << std::endl;
+  // std::cout << get_indexes_of(4,board_1) << std::endl;
+  // std::cout << get_indexes_of(5,board_1) << std::endl;
+  // std::cout << get_indexes_of(6,board_1) << std::endl;
+  snakes_and_ladders(board_2);
 
   return 0;
 }
